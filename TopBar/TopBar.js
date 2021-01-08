630e0db46afd04e5964f1e88d87faf13c91ff655
saurabh630 <72503939+saurabh630@users.noreply.github.com>
2021-01-08 19:06:02 +0530

    Create TopBar.js

---
+++ TopBar/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TopBar.css'
+
+const TopBar = () =>{
+    return(
+        <header className="App-header">
+        <nav className="Topbar">  
+            <img src="https://i.dlpng.com/static/png/197684_preview.png" / >
+          </nav>
+        </header>  
+    );
+}
+
+export default TopBar; 
