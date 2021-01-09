d1ad18a7d0a031bf201644ff65d8f5605c6a18b8
saurabh630 <72503939+saurabh630@users.noreply.github.com>
2021-01-09 10:49:40 +0530

    Create ProductPreview.js

---
+++ ProductPreview/ProductPreview.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './ProductPreview.css';
+
+const ProductPreview = (props) =>{
+
+    const currentHour = new Date().getHours()>9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes()>9 ? new Date().getMinutes() : '0' + new Date().getMinutes(); 
+    return(
+        <div className={classes.ProductPreview}>
+            <img src="https://imgur.com/iOeUBV7.png"  />
+
+            {/* <div className={classes.TimeSection}>
+                <p>{currentHour}:{currentMinute}</p>
+                </div> */}
+
+                <div className={classes.HeartBeatSection}>
+                    <i class="fas fa-heartbeat"></i>
+                    <p>78</p>
+                </div>
+        </div>
+    );
+}
+
+export default ProductPreview;
